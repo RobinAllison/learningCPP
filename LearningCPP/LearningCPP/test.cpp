2b863f77a3baf57eff0468f366a85c0b254eedd4
Robin Allison <robinps2@sbcglobal.net>
2019-03-04 08:55:10 -0800

    trying to move test.cpp to the Chapter2 folder

---
+++ LearningCPP/LearningCPP/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  LearningCPP
+//
+//  Created by Robin Allison on 3/3/19.
+//  Copyright © 2019 Robin Allison. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ LearningCPP/LearningCPP/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  LearningCPP
+//
+//  Created by Robin Allison on 3/3/19.
+//  Copyright © 2019 Robin Allison. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
