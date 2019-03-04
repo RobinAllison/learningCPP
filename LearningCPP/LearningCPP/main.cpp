a35e6ab259756758dd9b95cb66aeb27b8142b5d3
Robin Allison <robinps2@sbcglobal.net>
2019-03-03 21:14:29 -0800

    created an initial Xcode project

---
+++ LearningCPP/LearningCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningCPP
+//
+//  Created by Robin Allison on 3/3/19.
+//  Copyright © 2019 Robin Allison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
